5e0429071d5ce10d97f5655e872994f69f7f8f37
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-06-29 13:35:33 -0500

    sure

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
-
+import ThoughtForm from '../components/ThoughtForm';
 import Auth from '../utils/auth';
 import { useQuery } from '@apollo/client';
 import { QUERY_THOUGHTS, QUERY_ME_BASIC } from '../utils/queries';
